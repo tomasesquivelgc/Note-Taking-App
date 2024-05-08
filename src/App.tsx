6a30893899c9025fc19ab697b233782fc56db10e
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-05-08 17:57:14 -0300

    wrap app component

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<Button>Hello World</Button>} />
-      <Route path="/new" element={<h1>This is the new section</h1>} />
-      <Route path="/:id">
-        <Route index element={<h1>Show</h1>} />
-        <Route path="edit" element={<h1>Edit</h1>} />
-      </Route>
-      <Route path="*" element={<Navigate to="/"/>} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Button>Hello World</Button>} />
+        <Route path="/new" element={<h1>This is the new section</h1>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/"/>} />
+      </Routes>
+    </>
   )
 }
 
