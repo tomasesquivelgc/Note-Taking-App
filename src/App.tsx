1b21d2f74ee6c487a320a0d793cd6d38bd9b9a47
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-05-08 17:56:45 -0300

    create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import Button from '@mui/material/Button';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-      <Button variant="contained">Hello world</Button>
-    </>
+    <Routes>
+      <Route path="/" element={<Button>Hello World</Button>} />
+      <Route path="/new" element={<h1>This is the new section</h1>} />
+      <Route path="/:id">
+        <Route index element={<h1>Show</h1>} />
+        <Route path="edit" element={<h1>Edit</h1>} />
+      </Route>
+      <Route path="*" element={<Navigate to="/"/>} />
+    </Routes>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,12 @@ import '@fontsource/roboto/700.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
